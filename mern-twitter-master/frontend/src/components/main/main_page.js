358a8561e00209c8a8dcdee489183f57e26ecbca
Jon Christie <jonpchristie@gmail.com>
2020-08-19 09:58:34 -0400

    initial

---
+++ mern-twitter-master/frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Cloning things to learn</p>
+        <footer>
+           
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
